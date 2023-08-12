2d9e5570db232a2b94974fb192d5b695a500ddc1
crowdigit <87285844+crowdigit@users.noreply.github.com>
2023-08-13 02:27:56 +0900

    chore: remove not used code

--- shader/frag.glsl
+++ shader/frag.glsl
@@ -3,10 +3,8 @@
 in vec2 texCoord;
 out vec4 out_color;
 
-uniform vec3 color;
 uniform sampler2D sampler;
 
 void main() {
     out_color = texture(sampler, texCoord);
-    // out_color = vec4(color, 0.0);
 }
